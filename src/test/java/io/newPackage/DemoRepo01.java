791eecacb5a6e53e34a3e8a76376799cf70278a6
emirhan <emirhan190341@gmail.com>
2022-07-05 11:29:54 +0300

    Add a second test

---
+++ src/test/java/io/newPackage/DemoRepo01.java
@@ -0,0 +1,18 @@
+package io.newPackage;
+
+
+import org.testng.annotations.Test;
+
+public class DemoRepo01 {
+    @Test
+    public void emirhanTest(){
+
+
+    }
+
+    @Test
+    public void emirhanTest1(){
+
+    }
+
+}
